059616cdfa3b18e73c148bd4616d92ed01d8c3f3
Dave Bennett <david.a.bennett7@gmail.com>
2021-07-25 11:59:44 -0400

    comments

--- start_soil_service.sh
+++ start_soil_service.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # in virtualenvrionment we install
-# pip install pymongo==3.4.0
 # pip install pigpio
 /home/pi/git/soil_moisture_sensor/venv/bin/python3 /home/pi/git/soil_moisture_sensor/receiver.py
